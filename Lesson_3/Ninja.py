a7abd07c3aff61355832bd3a8991d3b6565a0c8a
B0GDAN <bogdanfesil@gmail.com>
2022-10-01 20:24:27 +0300

    Код полностью завершен

--- Lesson_3/Ninja.py
+++ Lesson_3/Ninja.py
@@ -15,3 +15,18 @@ class Ninja(Character):
    def attack(self, target):
 
        if random.randint(1, 100) > 50:
+           target.take_damage(self.damage + self.count_addition_damage())
+
+           else:
+
+           print('Ниндзя увернулся от удара!')
+
+   def take_damage(self, damage):
+
+       if random.randint(1, 100) > 50:
+
+           self.hp -= damage
+
+       else:
+
+           print('Ниндзя увернулся от удара!')
