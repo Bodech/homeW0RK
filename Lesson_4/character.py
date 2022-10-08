a1db0fd6e2fe7b1079ca00ecb0289a79dd5321b1
B0GDAN <bogdanfesil@gmail.com>
2022-10-08 17:22:53 +0300

    Добавлен новое оружие - Булова

---
+++ Lesson_4/character.py
@@ -0,0 +1,27 @@
+from Lesson_2.Character import Character
+from item import Item
+from enum import Enum, auto
+
+class CharacterStats(Enum):
+    HP = auto()
+    DAMAGE = auto()
+    DEFENCE = auto()
+
+class CharacterWithItems(Character):
+    def __init__(self, name, hp, damage, defence):
+        Character.__init__(self, name, hp, damage, defence)
+        self.weapon = None
+        self.armor = None
+
+    def set_weapon(self, item: Item = None):
+        self.weapon = item
+
+    def set_armor(self, item: Item = None):
+        self.armor = item
+
+    def attack(self, target):
+        try:
+            additional_damage = self.weapon.use()[CharacterStats.DAMAGE]
+        except Exception as error:
+            additional_damage = 0
+        target.take_damage(self.damage + additional_damage)
---
+++ Lesson_4/main.py
@@ -0,0 +1,33 @@
+from character import CharacterWithItems
+from item import Item
+from character import CharacterStats as stat
+
+potion1 = Item('Зелье силы',
+               stats={
+                   stat.DAMAGE: 2,
+                   stat.DEFENCE: 2
+               })
+weapon1 = Item('Стальной меч', stats={stat.DAMAGE: 4},
+               durability=1)
+armor1 = Item('Кожанная броня', stats={stat.DEFENCE: 2},
+              durability=400)
+weapon2 = Item('Булава', stats={stat.DAMAGE: 8},
+            durability=10)
+
+player1 = CharacterWithItems(
+    name='Vasya', hp=30, damage=1, defence=0)
+player2 = CharacterWithItems(
+    name='Petya', hp=30, damage=1, defence=0)
+
+player1.set_weapon(weapon1)
+player2.set_weapon(weapon2)
+
+print(player1)
+print(player2)
+
+while player1.hp > 0 and player2.hp > 0:
+    player1.attack(player2)
+    player2.attack(player1)
+
+    print(player1)
+    print(player2)
